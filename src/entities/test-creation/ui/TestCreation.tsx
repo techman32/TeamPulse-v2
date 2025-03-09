fdd8920fa56f2f6ce239a83567819708fe944656
techman32 <klochko977@gmail.com>
2025-03-09 22:28:27 +0300

    Добавил возможность задавать иконки кнопкам, добавил кнопку Импорта в создании теста

--- src/entities/test-creation/ui/TestCreation.tsx
+++ src/entities/test-creation/ui/TestCreation.tsx
@@ -4,6 +4,7 @@ import Button from '@/shared/ui/Button'
 import Textarea from '@/shared/ui/Textarea'
 import TestFormWithTheme from '@/entities/test-creation/ui/TestFormWithTheme'
 import {useTestCreationStore} from '@/entities/test-creation/model/store'
+import {Import} from 'lucide-react'
 
 export default function TestCreation() {
   const store = useTestCreationStore()
@@ -14,7 +15,10 @@ export default function TestCreation() {
 
   return (
     <div className={'flex flex-col gap-4'}>
-      <h1 className={'font-bold text-xl'}>Создание теста</h1>
+      <div className={'flex justify-between flex-wrap gap-4'}>
+        <h1 className={'font-bold text-2xl'}>Создание теста</h1>
+        <Button text={'Импортировать'} color={'secondary'} icon={<Import size={16}/>}/>
+      </div>
       <div className={'flex flex-col gap-2'}>
         <h2 className={'font-semibold'}>Название</h2>
         <Input
@@ -33,7 +37,7 @@ export default function TestCreation() {
         <h2 className={'font-semibold'}>Тест</h2>
         <TestFormWithTheme/>
       </div>
-      <div className={'flex gap-2'}>
+      <div className={'flex gap-2 flex-wrap'}>
         <Button text={'Опубликовать'} onClick={handleSubmit}/>
         <Button text={'Добавить в черновик'} color={'secondary'}/>
       </div>
--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -1,22 +1,29 @@
-import {ButtonHTMLAttributes} from 'react'
+import {ButtonHTMLAttributes, ReactNode} from 'react'
 import cn from 'classnames'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string,
   color?: 'primary' | 'secondary' | 'danger'
+  icon?: ReactNode
 }
 
-export default function Button({text, color = 'primary', ...props}: ButtonProps) {
+export default function Button({text, color = 'primary', icon, ...props}: ButtonProps) {
   return (
     <button
       {...props}
-      className={cn('rounded-md min-w-[180px] px-4 py-2 transition-all cursor-pointer', {
+      className={cn('relative rounded-md min-w-[180px] px-4 py-2 transition-all cursor-pointer', {
         'bg-black text-white hover:bg-black/85 active:bg-black/80': color === 'primary',
         'bg-white border border-gray-200 text-black hover:bg-gray-50 active:bg-gray-100': color === 'secondary',
-        'bg-red-500 text-white hover:bg-red-500/85 active:bg-red-500/80': color === 'danger'
+        'bg-red-500 text-white hover:bg-red-500/85 active:bg-red-500/80': color === 'danger',
+        'pl-8 pr-4': icon
       })}
     >
       {text}
+      {icon && (
+        <span className={'absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none'}>
+          {icon}
+        </span>
+      )}
     </button>
   )
 }
