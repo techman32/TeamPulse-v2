fec600c6f90716a1901881ef9de6e0012c100691
techman32 <klochko977@gmail.com>
2025-03-09 19:12:21 +0300

    Добавил страницу дашборда и страницу создания теста

---
+++ src/app/dashboard/[test]/create/page.tsx
@@ -0,0 +1,7 @@
+export default function TestCreatePage() {
+  return (
+    <div className={'border border-gray-200 rounded-md mx-[15%] my-4 py-4 px-6 shadow-xs flex flex-col gap-4'}>
+      <h1>Страница создание теста</h1>
+    </div>
+  )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className={'border border-gray-200 rounded-md mx-[15%] my-4 py-4 px-6 shadow-xs flex flex-col gap-4'}>
+      <h1>Страница дашборда</h1>
+    </div>
+  )
+}
