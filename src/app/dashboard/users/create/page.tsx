931ef7279e8a2d25af5f9ae044c8f00e276814dd
techman32 <klochko977@gmail.com>
2025-03-10 14:40:31 +0300

    Страница создания пользователя

---
+++ src/app/dashboard/users/create/page.tsx
@@ -0,0 +1,9 @@
+import UserCreation from '@/entities/user-creation/ui/UserCreation'
+
+export default function UserCreationPage() {
+  return (
+    <div className={'border border-gray-200 rounded-md mx-[8%] my-4 py-4 px-6 shadow-xs'}>
+      <UserCreation/>
+    </div>
+  )
+}
